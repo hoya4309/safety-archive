7dc3d339436a0e7b1d20995309a6d44d368eea83
hoya4309 <127062220+hoya4309@users.noreply.github.com>
2026-09-23 10:29:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let archiveData = [];
 fetch("data/archive.json")
     .then(response => response.json())
     .then(data => {
-
         archiveData = data;
-
         renderData(data);
     });
 
@@ -18,47 +16,34 @@ function renderData(data) {
     data.forEach(item => {
 
         gallery.innerHTML += `
-        
         <div class="card">
 
             <h3>${item.title}</h3>
 
-            <p>구분 : ${item.type}</p>
-
-            <p>위험유형 : ${item.category}</p>
-
-            <p>현장 : ${item.site}</p>
-
-            <p>날짜 : ${item.date}</p>
+            <p><strong>구분 :</strong> ${item.type}</p>
+            <p><strong>위험유형 :</strong> ${item.category}</p>
+            <p><strong>현장 :</strong> ${item.site}</p>
+            <p><strong>날짜 :</strong> ${item.date}</p>
 
             ${item.photo}
 
             <p>${item.description}</p>
 
         </div>
-
         `;
     });
 }
 
-document
-.getElementById("search")
-.addEventListener("input", function() {
+document.getElementById("search")
+.addEventListener("input", function(){
 
-    const keyword =
-    this.value.toLowerCase();
+    const keyword = this.value.toLowerCase();
 
-    const filtered =
-    archiveData.filter(item =>
-
+    const filtered = archiveData.filter(item =>
         item.title.toLowerCase().includes(keyword) ||
-
         item.site.toLowerCase().includes(keyword) ||
-
         item.category.toLowerCase().includes(keyword)
-
     );
 
     renderData(filtered);
-
 });
