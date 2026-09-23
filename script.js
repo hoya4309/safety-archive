9068d1e658635c1dfc38be823d14ca424dbb52d0
hoya4309 <127062220+hoya4309@users.noreply.github.com>
2026-09-23 10:11:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
+let archiveData = [];
+
 fetch("data/archive.json")
     .then(response => response.json())
     .then(data => {
 
-        const gallery = document.getElementById("gallery");
+        archiveData = data;
 
-        data.forEach(item => {
-
-            gallery.innerHTML += `
-            
-            <div class="card">
-
-                <h3>${item.title}</h3>
-
-                <p><strong>구분 :</strong> ${item.type}</p>
-
-                <p><strong>위험유형 :</strong> ${item.category}</p>
-
-                <p><strong>현장 :</strong> ${item.site}</p>
-
-                <p><strong>날짜 :</strong> ${item.date}</p>
-
-                ${item.photo}
-
-                <p>${item.description}</p>
-
-            </div>
-
-            `;
-        });
-
+        renderData(data);
     });
+
+function renderData(data) {
+
+    const gallery = document.getElementById("gallery");
+
+    gallery.innerHTML = "";
+
+    data.forEach(item => {
+
+        gallery.innerHTML += `
+        
+        <div class="card">
+
+            <h3>${item.title}</h3>
+
+            <p>구분 : ${item.type}</p>
+
+            <p>위험유형 : ${item.category}</p>
+
+            <p>현장 : ${item.site}</p>
+
+            <p>날짜 : ${item.date}</p>
+
+            ${item.photo}
+
+            <p>${item.description}</p>
+
+        </div>
+
+        `;
+    });
+}
+
+document
+.getElementById("search")
+.addEventListener("input", function() {
+
+    const keyword =
+    this.value.toLowerCase();
+
+    const filtered =
+    archiveData.filter(item =>
+
+        item.title.toLowerCase().includes(keyword) ||
+
+        item.site.toLowerCase().includes(keyword) ||
+
+        item.category.toLowerCase().includes(keyword)
+
+    );
+
+    renderData(filtered);
+
+});
